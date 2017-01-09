055a8a638da64808180bb98f1515891761ab7c0e
Moataz Abdelmageed <Moataz Abdelmageed>
2017-01-09 16:31:14 +0200

    create users table

---
+++ js/components/userComponent.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Moataz on 1/9/2017.
+ */
+(function () {
+    'use strict';
+
+    var app = angular.module('app');
+
+    app.component('userInfo', {
+
+
+        // Binds caption to the parent scope
+        bindings: {
+            caption: '<'
+        },
+        // Loads the component template
+        templateUrl: '/js/components/userComponent.html',
+        controller: function () {
+
+            // The list of users we will be displaying
+            this.records = [{
+                name: "Alfreds Futterkiste",
+                city: "Berlin",
+                Country: "Germany"
+            }, {
+                name: "Ana Trujillo Emparedados y helados",
+                city: "México D.F.",
+                country: "Mexico"
+            }, {
+                name: "Blondel père et fils",
+                city: "Strasbourg",
+                country: "France"
+            }, {
+                name: "Moataz",
+                city: "Giza",
+                country: "Egypt"
+            }, {
+                name: "Bólido Comidas preparadas",
+                city: "Madrid",
+                country: "Spain"
+            }];
+        }
+    });
+})();
